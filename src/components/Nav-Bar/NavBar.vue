f3a4ba124164383498adf939e7099812f0d0e1cd
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-07-03 23:30:48 +0800

    connecting global store with navBar

---
+++ src/components/Nav-Bar/NavBar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { computed, toRefs } from "vue";
+import store from "../../store/index.ts";
+
+const { activeLanguage, languages, toggleLanguage } = store.value;
+
+const { navBar } = toRefs(languages);
+
+const languageName = computed(() => {
+  return activeLanguage === "en" ? "EN/ES" : "ES/EN";
+});
+
+</script>
+
+<template>
+  <nav class="header">
+    <div class="logo">
+      <p>https://Juan Gómez</p>
+    </div>
+    <ul class="options-container">
+      <li>{{ navBar.whoIAm}}</li>
+      <li>{{ navBar.projects }}</li>
+      <li>{{ navBar.contactMe }}</li>
+    </ul>
+    <button @click="toggleLanguage">{{ languageName }}</button>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+@import "./../../style.scss";
+</style>
