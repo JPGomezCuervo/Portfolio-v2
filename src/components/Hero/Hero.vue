f907a04258c9a67174bd0ef7df3e4cda5e30e40d
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-07-05 01:13:40 +0800

    Create Hero component

---
+++ src/components/Hero/Hero.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <section class="background">
+
+    </section>
+
+</template>
+
+<style lang="scss" scoped>
+@import "../../style.scss";
+.background {
+    height: 80vh;
+    width: 100%;
+}
+
+
+
+</style>
