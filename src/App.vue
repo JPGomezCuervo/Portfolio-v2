e7d9046c930beef92994cedf6d47b15157501376
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-07-05 18:54:10 +0800

    fixed bug in exports

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,12 @@ import Home from './views/Home.vue';
 <template>
   <NavBar/>
   <Home/>
-  <div class="lolis">
 
-  </div>
 </template>
 
 <style lang="scss" scoped>
 @import "./style.scss";
 
-.lolis {
-  height: 290vh;
-  width: 100%;
-  background-color: red;
-}
+
 
 </style>
