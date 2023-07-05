eb476c1134dc5040f7aa2a761872950506cfa95b
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-07-05 09:37:26 +0800

    lolis

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
+
 import NavBar from './components/Nav-Bar/NavBar.vue';
+import Hero from './components/Hero/Hero.vue';
+
 </script>
 
 <template>
   <NavBar/>
+  <Hero/>
+  <div class="lolis">
+
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "./style.scss";
+
+.lolis {
+  height: 290vh;
+  width: 100%;
+  background-color: red;
+}
 
 </style>
