095af6edb565d88fa43fb578cefe99780b0a1942
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-07-06 21:57:13 +0800

    Refactor index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,28 +5,36 @@ import en_hero from "../translations/en/hero.json";
 import es_hero from "../translations/es/hero.json";
 import en_whoIAm from "../translations/en/whoIAm.json";
 import es_whoIAm from "../translations/es/whoIAm.json";
-
+import { RootStore } from "./index.types";
 
 const toggleLanguage = () => {
-    store.value.activeLanguage = store.value.activeLanguage === "en" ? "es" : "en";
-    if (store.value.activeLanguage === "en") {
-        store.value.languages.navBar = en_navbar;
-        store.value.languages.hero = en_hero;
-        store.value.languages.whoIAm = en_whoIAm
-    } else {
-        store.value.languages.navBar = es_navbar;
-        store.value.languages.hero = es_hero;
-        store.value.languages.whoIAm = es_whoIAm
-    }
+  const { activeLanguage, languages } = store.value;
+
+  store.value.activeLanguage = activeLanguage === "en" ? "es" : "en";
+  
+  if (store.value.activeLanguage === "en") {
+    Object.assign(languages, {
+      navBar: en_navbar,
+      hero: en_hero,
+      whoIAm: en_whoIAm
+    });
+  } else {
+    Object.assign(languages, {
+      navBar: es_navbar,
+      hero: es_hero,
+      whoIAm: es_whoIAm
+    });
+  }
 };
-const store:Ref<RootStore> = ref({
-    activeLanguage: "en",
-    languages: {
-        navBar:en_navbar,
-        hero:en_hero,
-        whoIAm:en_whoIAm
-    },
-    toggleLanguage
+
+const store: Ref<RootStore> = ref({
+  activeLanguage: "en",
+  languages: {
+    navBar: en_navbar,
+    hero: en_hero,
+    whoIAm: en_whoIAm
+  },
+  toggleLanguage
 });
 
 export default store;
