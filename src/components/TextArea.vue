11237f54c40731283d93dc4f6e36485f657f4d7e
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-07-09 10:40:02 +0800

    implement custom Text Area

---
+++ src/components/TextArea.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { TextArea } from '../store/index.types';
+
+const props = defineProps<TextArea>();
+
+</script>
+
+<template>
+    <div :class="props.class">
+        <label 
+            v-bind:for="props.name">
+            {{ props.label }}
+        </label>
+
+        <textarea 
+            v-bind="$attrs"
+            :name="props.name"
+            :placeholder="props.placeholder"
+            :value="props.modelValue"
+            @input="$emit('update:modelValue', ($event.target as HTMLTextAreaElement).value)"
+            >
+            
+
+        </textarea>
+
+    </div>
+
+</template>
