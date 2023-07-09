e53b5258656c4873fa551bfb7181ab3509f9ca99
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-07-09 09:50:47 +0800

    implement a BaseInput component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { BaseInput } from '../store/index.types';
+
+const props = defineProps<BaseInput>();
+
+
+</script>
+
+<template>
+    <div v-bind:class="props.class">
+        <label v-bind:for="props.name">{{ props.label }}</label>
+        <input 
+            v-bind="$attrs"
+            :placeholder="props.placeholder" 
+            :type="props.type" 
+            :name="props.name" 
+            :value="props.modelValue"
+            @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+            />
+    </div>
+</template>
