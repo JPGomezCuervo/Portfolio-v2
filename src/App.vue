00528eb1e34ec73c66df1f85abf866f72db6c7c3
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-07-10 09:32:34 +0800

    Add Footer to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 
-import NavBar from './components/TheNavBar.vue';
-import Home from './views/Home.vue';
+import NavBar from "./components/TheNavBar.vue";
+import Home from "./views/Home.vue";
+import Footer from "./components/TheFooter.vue";
 </script>
 
 <template>
   <NavBar/>
   <Home/>
+  <Footer/>
 </template>
 
 <style lang="scss" scoped>
