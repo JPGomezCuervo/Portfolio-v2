8ef7fce4e098b6906f41a43cf839142a07bc0f58
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-07-12 22:09:00 +0800

    fix gap in tags

--- src/components/Tags.vue
+++ src/components/Tags.vue
@@ -10,14 +10,19 @@ const type = (props.kind === "back") ? "back" : "front";
 </script>
 
 <template>
-    <div>
+    <div class="lolis">
         <span v-for="tag in props.tags" :class="`${type} tag-container`">{{ tag }}</span>
     </div>
 
 </template>
 
 <style>
-    
+    .lolis {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: left;
+        align-items: center;
+    }
     .tag-container {
         margin: 0 .3rem;
         padding: .3rem;
@@ -28,9 +33,11 @@ const type = (props.kind === "back") ? "back" : "front";
         background-color: #6F4AE7;
         font-family: "Montserrat-Medium";
         color: white;
+        margin-bottom: 1%;
     }
     
     .back  {
+        margin-bottom: 2%;
         background-color: #FD6439;
         padding: 5px;
         font-family: "Montserrat-Medium";
