d99938db3c624e6def65610325748a3b2dcf922f
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-07-12 09:55:42 +0800

    Restructure tag component

--- src/components/Tags.vue
+++ src/components/Tags.vue
@@ -1,55 +1,38 @@
 <script setup lang="ts">
 const props = defineProps<{
     tags?: string[];
-    style: "back" | "front";
+    kind: "back" | "front";
 }>();
 
-const type = (props.style === "back") ? "back" : "front";
+const type = (props.kind === "back") ? "back" : "front";
+
 
 </script>
 
 <template>
-    <div v-for="tag in props.tags" :class="type">
-        <span>{{ tag }}</span>
-
+    <div>
+        <span v-for="tag in props.tags" :class="`${type} tag-container`">{{ tag }}</span>
     </div>
 
 </template>
 
 <style>
-
-    .front {
-        display: flex;
-        flex-wrap: wrap;
-    }
-
-    .front div {
-        background-color: #6F4AE7;
-        margin-right: 1rem;
-        margin-bottom: 1rem;
+    
+    .tag-container {
+        margin: 0 .3rem;
         padding: .3rem;
 
     }
-
-    .front span {
+    
+    .front  {
+        background-color: #6F4AE7;
         font-family: "Montserrat-Medium";
         color: white;
     }
-
-    .back {
-        display: flex;
-        flex-wrap: wrap;
-    }
-    .back div {
+    
+    .back  {
         background-color: #FD6439;
-        margin-right: 1rem;
-        margin-bottom: 1rem;
-        padding: .3rem;
-
-    }
-
-
-    .back span {
+        padding: 5px;
         font-family: "Montserrat-Medium";
         color: white;
     }
